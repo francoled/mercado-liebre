652d332b85b8d33d45286ebe035f972ab65446cd
francoled <ledesmaf659@gmail.com>
2023-11-13 23:23:51 -0300

    agregando packagejson

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const path=require('path');
 const app=express();
 const publicpath=path.resolve(__dirname,'./public')
 app.use(express.static('public'));
-app.listen(3000,() => {
+const port=process.env.PORT || 3001;
+app.listen(port,() => {
 
-    console.log("servidor corriendo en el puerto 3000")
+    console.log("servidor corriendo en el puerto")
 })
 app.get('/',(req, res)=>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
