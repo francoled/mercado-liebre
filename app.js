dd2338abbc0a60663d01c1586e48dbd6be30ee6e
francoled <ledesmaf659@gmail.com>
2023-11-13 21:52:13 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+const publicpath=path.resolve(__dirname,'./public')
+app.use(express.static('public'));
+app.listen(3000,() => {
+
+    console.log("servidor corriendo en el puerto 3000")
+})
+app.get('/',(req, res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
